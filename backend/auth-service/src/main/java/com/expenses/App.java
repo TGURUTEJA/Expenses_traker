5078e06e709ba43fb856a9c31104e0e1c602a356
Thondamanadu guruteja <75000532+TGURUTEJA@users.noreply.github.com>
2025-05-12 10:06:08 +0000

    auth-services setup

---
+++ backend/auth-service/src/main/java/com/expenses/App.java
@@ -0,0 +1,13 @@
+package com.expenses;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
